bd40799536038049eaebe76de696e903d366c951
mpiech1813 <maciejpiech18@gmail.com>
2021-10-05 11:22:27 -0500

    started writing the react component but need html file first

--- frontend/clicker/index.js
+++ frontend/clicker/index.js
@@ -1,2 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+class Clicker extends Component {
+    constructor() {
+        super();
+        this.state = {
+            counter: 0,
+        };
+    }
+
+    render() {
+        const { counter } = this.state;
+
+        return <div></div>;
+    }
+}
