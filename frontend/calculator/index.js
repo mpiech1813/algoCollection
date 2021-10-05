52fa910e99803f283076cae508be1b5ca4cd8d84
mpiech1813 <maciejpiech18@gmail.com>
2021-10-05 18:14:22 -0500

    good point to save

--- frontend/calculator/index.js
+++ frontend/calculator/index.js
@@ -5,44 +5,88 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            data=''
+            data: '',
         };
     }
 
-    handleClick= (e) =>{
-        const value = e.target
-        console.log(value)
-    }
+    handleClick = (e) => {
+        const value = e.target.getAttribute('value');
+        let { data } = this.state;
+
+        data += value;
+        this.setState({ data });
+    };
+
+    handleClear = () => {
+        this.setState({ data: '' });
+    };
+
+    handleCheck = () => {
+        const { data } = this.state;
+        console.log(data);
+    };
 
     render() {
         return (
             <div>
-                <h3>Result: {}</h3>
+                <h3>Result: </h3>
                 <div>
-                    <button onClick={this.handleClick} value='1'>1</button>
-                    <button onClick={this.handleClick} value='2'>2</button>
-                    <button onClick={this.handleClick} value='3'>3</button>
+                    <button onClick={this.handleClick} value='1'>
+                        1
+                    </button>
+                    <button onClick={this.handleClick} value='2'>
+                        2
+                    </button>
+                    <button onClick={this.handleClick} value='3'>
+                        3
+                    </button>
                 </div>
                 <div>
-                    <button onClick={this.handleClick} value='4'>4</button>
-                    <button onClick={this.handleClick} value='5'>5</button>
-                    <button onClick={this.handleClick} value='6'>6</button>
+                    <button onClick={this.handleClick} value='4'>
+                        4
+                    </button>
+                    <button onClick={this.handleClick} value='5'>
+                        5
+                    </button>
+                    <button onClick={this.handleClick} value='6'>
+                        6
+                    </button>
                 </div>
                 <div>
-                    <button onClick={this.handleClick} value='7'>7</button>
-                    <button onClick={this.handleClick} value='8'>8</button>
-                    <button onClick={this.handleClick} value='9'>9</button>
+                    <button onClick={this.handleClick} value='7'>
+                        7
+                    </button>
+                    <button onClick={this.handleClick} value='8'>
+                        8
+                    </button>
+                    <button onClick={this.handleClick} value='9'>
+                        9
+                    </button>
                 </div>
                 <div>
-                    <button onClick={this.handleClick} value='0'>0</button>
-                    <button onClick={this.handleClick} value='-'>-</button>
-                    <button onClick={this.handleClick} value='+'>+</button>
-                    <button onClick={this.handleClick} value='*'>*</button>
-                    <button onClick={this.handleClick} value='/'>/</button>
+                    <button onClick={this.handleClick} value='0'>
+                        0
+                    </button>
+                    <button onClick={this.handleClick} value='-'>
+                        -
+                    </button>
+                    <button onClick={this.handleClick} value='+'>
+                        +
+                    </button>
+                    <button onClick={this.handleClick} value='*'>
+                        *
+                    </button>
+                    <button onClick={this.handleClick} value='/'>
+                        /
+                    </button>
                 </div>
                 <div>
-                    <button onClick={this.handleClick} value='equal'>=</button>
-                    <button onClick={this.handleClick} value='clear'>C</button>
+                    <button onClick={this.handleCheck} value='equal'>
+                        =
+                    </button>
+                    <button onClick={this.handleClear} value='clear'>
+                        C
+                    </button>
                 </div>
             </div>
         );
