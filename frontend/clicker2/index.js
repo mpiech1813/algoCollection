04b19e53b9751b7222ec3ddd4c6ed10e702aae95
mpiech1813 <maciejpiech18@gmail.com>
2021-10-05 12:26:38 -0500

    final solution

--- frontend/clicker2/index.js
+++ frontend/clicker2/index.js
@@ -4,13 +4,25 @@ import ReactDOM from 'react-dom';
 class App extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            counter: 0,
+        };
     }
 
+    clickAction = () => {
+        let counter = this.state.counter;
+        counter++;
+        this.setState({ counter });
+    };
+
     render() {
+        const { counter } = this.state;
+        const { clickAction } = this;
+
         return (
             <div>
-                <h1>Hello from this container</h1>
+                <h1>Number of clicks: {counter}</h1>
+                <button onClick={clickAction}>Click Me</button>
             </div>
         );
     }
