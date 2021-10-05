224184b9aa88ed596960d1d8fe45874f2fdb930c
mpiech1813 <maciejpiech18@gmail.com>
2021-10-05 17:54:21 -0500

    added the buttons

--- frontend/calculator/index.js
+++ frontend/calculator/index.js
@@ -4,13 +4,46 @@ import ReactDOM from 'react-dom';
 class App extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            data=''
+        };
+    }
+
+    handleClick= (e) =>{
+        const value = e.target
+        console.log(value)
     }
 
     render() {
         return (
             <div>
-                <h1>Hello from the component</h1>
+                <h3>Result: {}</h3>
+                <div>
+                    <button onClick={this.handleClick} value='1'>1</button>
+                    <button onClick={this.handleClick} value='2'>2</button>
+                    <button onClick={this.handleClick} value='3'>3</button>
+                </div>
+                <div>
+                    <button onClick={this.handleClick} value='4'>4</button>
+                    <button onClick={this.handleClick} value='5'>5</button>
+                    <button onClick={this.handleClick} value='6'>6</button>
+                </div>
+                <div>
+                    <button onClick={this.handleClick} value='7'>7</button>
+                    <button onClick={this.handleClick} value='8'>8</button>
+                    <button onClick={this.handleClick} value='9'>9</button>
+                </div>
+                <div>
+                    <button onClick={this.handleClick} value='0'>0</button>
+                    <button onClick={this.handleClick} value='-'>-</button>
+                    <button onClick={this.handleClick} value='+'>+</button>
+                    <button onClick={this.handleClick} value='*'>*</button>
+                    <button onClick={this.handleClick} value='/'>/</button>
+                </div>
+                <div>
+                    <button onClick={this.handleClick} value='equal'>=</button>
+                    <button onClick={this.handleClick} value='clear'>C</button>
+                </div>
             </div>
         );
     }
