4c820c65aad39791cac6d04de5a2e0dd36edf6b9
mpiech1813 <maciejpiech18@gmail.com>
2021-10-12 09:26:06 -0500

    created the blank component

---
+++ frontend/calculator2/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello from the component</h1>
+            </div>
+        );
+    }
+}
