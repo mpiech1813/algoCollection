65bb62971019ed351ff02a7b8d9713aac8c3bd58
mpiech1813 <maciejpiech18@gmail.com>
2021-10-12 09:44:41 -0500

    added buttons

---
+++ frontend/calculator2/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /home/robak/Documents/Programowanie/algos/frontend/calculator2/index.js: JSX attributes must only be assigned a non-empty expression. (19:36)\n\n\u001b[0m \u001b[90m 17 |\u001b[39m                 \u001b[33m<\u001b[39m\u001b[33mh1\u001b[39m\u001b[33m>\u001b[39m\u001b[33mDisplay\u001b[39m\u001b[33m:\u001b[39m {display}\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mh1\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 18 |\u001b[39m                 \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 19 |\u001b[39m                     \u001b[33m<\u001b[39m\u001b[33mbutton\u001b[39m onClick\u001b[33m=\u001b[39m{} value\u001b[33m=\u001b[39m\u001b[32m'1'\u001b[39m\u001b[33m>\u001b[39m\u001b[35m1\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mbutton\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m                                     \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 20 |\u001b[39m                     \u001b[33m<\u001b[39m\u001b[33mbutton\u001b[39m onClick\u001b[33m=\u001b[39m{} value\u001b[33m=\u001b[39m\u001b[32m'2'\u001b[39m\u001b[33m>\u001b[39m\u001b[35m2\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mbutton\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 21 |\u001b[39m                     \u001b[33m<\u001b[39m\u001b[33mbutton\u001b[39m onClick\u001b[33m=\u001b[39m{} value\u001b[33m=\u001b[39m\u001b[32m'3'\u001b[39m\u001b[33m>\u001b[39m\u001b[35m3\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mbutton\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 22 |\u001b[39m                 \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n    at Object._raise (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:541:17)\n    at Object.raiseWithData (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:534:17)\n    at Object.raise (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:495:17)\n    at Object.jsxParseAttributeValue (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:7276:16)\n    at Object.jsxParseAttribute (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:7325:38)\n    at Object.jsxParseOpeningElementAfterName (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:7345:28)\n    at Object.jsxParseOpeningElementAt (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:7338:17)\n    at Object.jsxParseElementAt (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:7370:33)\n    at Object.jsxParseElementAt (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:7386:32)\n    at Object.jsxParseElementAt (/home/robak/Documents/Programowanie/algos/frontend/calculator2/node_modules/@babel/parser/lib/index.js:7386:32)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
--- frontend/calculator2/index.js
+++ frontend/calculator2/index.js
@@ -4,13 +4,43 @@ import ReactDOM from 'react-dom';
 class App extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            display: '',
+        };
     }
 
     render() {
+        const display = this.state;
+
         return (
             <div>
-                <h1>Hello from the component</h1>
+                <h1>Display: {display}</h1>
+                <div>
+                    <button onClick={} value='1'>1</button>
+                    <button onClick={} value='2'>2</button>
+                    <button onClick={} value='3'>3</button>
+                </div>
+                <div>
+                    <button onClick={} value='4'>4</button>
+                    <button onClick={} value='5'>5</button>
+                    <button onClick={} value='6'>6</button>
+                </div>
+                <div>
+                    <button onClick={} value='7'>7</button>
+                    <button onClick={} value='8'>8</button>
+                    <button onClick={} value='9'>9</button>
+                </div>
+                <div>
+                    <button onClick={} value='0'></button>
+                    <button onClick={} value='equal'>=</button>
+                    <button onClick={} value='clear'>C</button>
+                </div>
+                <div>
+                    <button onClick={} value='+'>+</button>
+                    <button onClick={} value='-'>-</button>
+                    <button onClick={} value='*'>*</button>
+                    <button onClick={} value='/'>/</button>
+                </div>
             </div>
         );
     }
