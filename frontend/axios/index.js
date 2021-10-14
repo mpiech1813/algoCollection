584aab32a327d7978bbd1742a33e7a9e9d0b1390
mpiech1813 <maciejpiech18@gmail.com>
2021-10-14 09:58:25 -0500

    created the component

---
+++ frontend/axios/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends Component {
+    render() {}
+}
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
