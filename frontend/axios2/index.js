bf0ed628369d50c4091f7cea0933d6c705202634
mpiech1813 <maciejpiech18@gmail.com>
2021-10-15 09:35:59 -0500

    added button and axios

--- frontend/axios2/index.js
+++ frontend/axios2/index.js
@@ -5,16 +5,27 @@ import axios from 'axios';
 class App extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = { pokemon: [] };
     }
 
-    getPokemon = () => {};
+    getPokemon = () => {
+        try {
+            const pokeList = (
+                await axios.get('https://pokeapi.co/api/v2/pokemon')
+            ).data;
+            this.setState({ pokemon: pokeList });
+        } catch (error) {
+            next(error);
+        }
+    };
 
     render() {
+        const getPokemon = this;
+
         return (
             <div>
                 <h1>Hello World</h1>
-                <button>Get the list</button>
+                <button onClick={getPokemon}>Get the list</button>
             </div>
         );
     }
