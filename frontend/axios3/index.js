d3307d060b5fb970947715321e25be61e92627cc
mpiech1813 <maciejpiech18@gmail.com>
2021-10-18 11:56:56 -0500

    polishing up a bug

--- frontend/axios3/index.js
+++ frontend/axios3/index.js
@@ -5,14 +5,32 @@ import axios from 'axios';
 class App extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = { pokeList: '' };
     }
 
+    getList = async () => {
+        try {
+            const list = (await axios.get('https://pokeapi.co/api/v2/pokemon'))
+                .data;
+            console.log(list);
+            this.setState({ pokeList: list.results });
+        } catch (error) {
+            next(error);
+        }
+    };
+
     render() {
+        const pokeList = this.state;
+
         return (
             <div>
                 <h1>hello from the Component</h1>
-                <button>Click Me!</button>
+                <button onClick={() => this.getList()}>Click Me!</button>
+                <ul>
+                    {pokeList.map((ele, idx) => {
+                        return <li key={idx}>{ele.name}</li>;
+                    })}
+                </ul>
             </div>
         );
     }
