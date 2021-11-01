fbd8065e83064c63b039c8d05ea8d2e9247a6676
mpiech1813 <maciejpiech18@gmail.com>
2021-11-01 13:04:43 -0500

    added the appraoch

--- leet/last_word.js
+++ leet/last_word.js
@@ -23,4 +23,14 @@ Input: s = "luffy is still joyboy"
 Output: 6
 Explanation: The last word is "joyboy" with length 6.
 
+A: 
+
+1) split the string into an array
+2) pop the last element off the array
+3) check if it is a empty space
+3 yes) discard and move to the next
+3 no) use this string
+4) try to use .lenght on this last element to see if the number of characters can be shown
+5) if above not working, split the elemnet into an array
+6) use .length to check the number of characters
  */
