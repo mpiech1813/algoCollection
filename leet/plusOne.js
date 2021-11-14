009ab3527dda1419558d541a5a1f1d34c6337bfa
mpiech1813 <maciejpiech18@gmail.com>
2021-11-13 18:45:18 -0600

    it is working

--- leet/plusOne.js
+++ leet/plusOne.js
@@ -93,4 +93,4 @@ const digits4 = [9];
 const digits5 = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
 const digits6 = [9, 9, 9, 9, 9];
 
-console.log(plusOne(digits5));
+console.log(plusOne(digits6));
