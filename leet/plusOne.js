eabccbfa7ce84d9de8b00703ff429f112cf21f5b
mpiech1813 <maciejpiech18@gmail.com>
2021-11-13 18:44:42 -0600

    new test

--- leet/plusOne.js
+++ leet/plusOne.js
@@ -91,5 +91,6 @@ const digits2 = [4, 3, 2, 1];
 const digits3 = [0];
 const digits4 = [9];
 const digits5 = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3];
+const digits6 = [9, 9, 9, 9, 9];
 
 console.log(plusOne(digits5));
