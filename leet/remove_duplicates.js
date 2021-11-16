d1ab1f0d23ca5e929c24ca85147d0fc9cb354edf
mpiech1813 <maciejpiech18@gmail.com>
2021-11-15 21:20:53 -0600

    reActo

--- leet/remove_duplicates.js
+++ leet/remove_duplicates.js
@@ -13,3 +13,13 @@
  * Input: head = [1,1,2,3,3]
  * Output: [1,2,3]
  */
+
+/**
+ * grab the current node    I
+ * grab the following node  II (let nextNode)
+ * compare the 2 values
+ * if not the same, move on (grab II and III)
+ * if the same, grab the following node (III)
+ * if node I and node III have diffrent values, assign I.next to node III
+ * if node I and node III have the same, grab node IV
+ */
