67b3e792ce0323fe03127960f81791635621765f
mpiech1813 <maciejpiech18@gmail.com>
2021-11-15 21:14:41 -0600

    Reacto

--- leet/remove_duplicates.js
+++ leet/remove_duplicates.js
@@ -1,1 +1,3 @@
 // Remove duplicates from sorted list
+
+//Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
