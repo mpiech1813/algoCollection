434a5663ca94533afe33255d84c91e954bab22f1
mpiech1813 <maciejpiech18@gmail.com>
2021-11-15 21:22:36 -0600

    created node

--- leet/remove_duplicates.js
+++ leet/remove_duplicates.js
@@ -23,3 +23,32 @@
  * if node I and node III have diffrent values, assign I.next to node III
  * if node I and node III have the same, grab node IV
  */
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
