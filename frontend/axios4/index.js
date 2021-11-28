3aa951fcde8db78c525bab92c43d31a3ad1973ff
mpiech1813 <maciejpiech18@gmail.com>
2021-11-27 19:29:11 -0600

    created the index.js file using hooks

---
+++ frontend/axios4/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    const [pokeList, setList] = useState;
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+const root = getElementById('app');
+
+ReactDOM.render(<App />, root);
