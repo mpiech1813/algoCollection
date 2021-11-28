9c41a18bb4ff9ae43621d75ca1ab49746ddfd57a
mpiech1813 <maciejpiech18@gmail.com>
2021-11-27 19:47:44 -0600

    list loaded on load

--- frontend/axios4/index.js
+++ frontend/axios4/index.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 function App() {
-    const [pokeList, setPokeList] = useState('');
+    const [pokeList, setPokeList] = useState([]);
 
-    useEffect(() => {});
+    useEffect(async () => {
+        const list = (await axios.get('https://pokeapi.co/api/v2/pokemon')).data
+            .results;
+        setPokeList(list);
+        console.log(pokeList);
+    }, []);
 
     return (
         <div>
             <h1>Hello World</h1>
+            <ul>
+                {pokeList.map((ele) => {
+                    return <li>{ele.name}</li>;
+                })}
+            </ul>
         </div>
     );
 }
