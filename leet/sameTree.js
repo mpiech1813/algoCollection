40512b343a2618e97f8cdce9fb01ed40e7d9cbaf
mpiech1813 <maciejpiech18@gmail.com>
2022-01-12 12:57:02 -0600

    R+E

---
+++ leet/sameTree.js
@@ -0,0 +1,29 @@
+/*
+R: 
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+E:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+                  1                                        1
+            2           3                               2       3
+
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+                1                                           1
+            2                                                   2
+
+
+            
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+                1                                           1
+            2       1                                   1       2
+*/
