b4c282049a7240dbcd81b553eab7dbb8c10f142b
mpiech1813 <maciejpiech18@gmail.com>
2022-01-12 19:15:10 -0600

    added else statement code

--- leet/lookUp.js
+++ leet/lookUp.js
@@ -36,6 +36,7 @@ function lookUpProfile(name, prop) {
       if (ele[prop] !== undefined) {
         finalResult = ele[prop];
       } else {
+        finalResult = "No such property";
       }
     }
   });
