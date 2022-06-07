86e090ea8e129c2970ad492e4f2a49f3f4e66271
mpiech1813 <maciejpiech18@gmail.com>
2022-06-07 09:37:31 -0500

    finished the instructions

---
+++ code_wars/morse2.ts
@@ -0,0 +1,22 @@
+// dot 1 => time unit
+// dash 3 => time units
+// pause between dots and dashes inside the character => 1 time unit
+// pause between characters in the word => 3 time units
+// pause between words => 7 units
+
+// 2 functions:
+// 1) decodeBits => find transmission rate, decode to dots, dashes and spaces(1 between character and 3 between words),
+// return result as string, ignore extra 0's 
+// 2) decode morseCode => translate from dots and dashes to human language => will just import from previous task to save time.
+
+import { MORSE_CODE } from './MORSE_CODE';
+
+export const decodeBits = (bits: string) => {
+  // ToDo: Accept 0's and 1's, return dots, dashes and spaces
+  return bits.replace('111', '-').replace('000', ' ').replace('1', '.').replace('0', '');
+};
+
+export const decodeMorse = (morseCode: string) => {
+  // ToDo: Accept dots, dashes and spaces, return human-readable message
+  return morseCode.replace('.', MORSE_CODE['.']).replace('-', MORSE_CODE['-']).replace(' ', '');
+};
