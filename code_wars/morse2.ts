808ca9241cacb9faecf9b8309a77b3a8ca71e5ad
mpiech1813 <maciejpiech18@gmail.com>
2022-06-08 10:50:48 -0500

    fixed the fox example

--- code_wars/morse2.ts
+++ code_wars/morse2.ts
@@ -22,6 +22,7 @@ export const decodeBits = (bits: string) => {
   // establish time unit dot
   let dot: string = ''
   let dash: string = ''
+  let space: string = ''
   let temp: string = ''
 
   // split bits into an array of individual characters to determine time unit
@@ -33,22 +34,32 @@ export const decodeBits = (bits: string) => {
       temp += '1'
     } else if (bitsArray[i] === '1' && bitsArray[i+1] === '0' || bitsArray[i] === '1' && bitsArray[i+1] === undefined){
       if(dot === ''){
-          temp += '1'
-          dot = temp
-          temp = ''
+        temp += '1'
+        dot = temp
+        temp = ''
       } else {
           temp += '1'
-          temp > dash ? dash = temp : ''
+          temp > dash && temp !== dot ? dash = temp : ''
           temp < dash && temp > dot ? dot = temp : ''
           temp = ''
       }
     }
   }
 
+  // make sure that dot is always smaller out of the two
+  if(dot.length > dash.length){
+    temp = dot
+    dot = dash
+    dash = temp
+  }
+
   const dots = new RegExp(dot, 'g')
   const dashes = new RegExp(dash, 'g')
   const spaces = new RegExp('0'.repeat(dot.length), 'g')
-
+ 
+  console.log('dot is: ', dot)
+  console.log('dash is: ', dash)
+  
   // split bits into an array of words in bits
   if(dash.length > 0){
     /**
@@ -79,8 +90,12 @@ const heyJudeBits2: string = '00000011001100110011000000110000001111110011001111
 const E1 = '1'
 const E2 = '111'
 const E3 = '01110'
-// console.log(decodeBits(E3))
-console.log(decodeMorse(decodeBits(E1)))
-console.log(decodeMorse(decodeBits(E2)))
-console.log(decodeMorse(decodeBits(E3)))
-console.log(decodeMorse(decodeBits(heyJudeBits2)))
+const I1 = '101'
+const I2 = '110011'
+const FOX = '00011100010101010001000000011101110101110001010111000101000111010111010001110101110000000111010101000101110100011101110111000101110111000111010000000101011101000111011101110001110101011100000001011101110111000101011100011101110001011101110100010101000000011101110111000101010111000100010111010000000111000101010100010000000101110101000101110001110111010100011101011101110000000111010100011101110111000111011101000101110101110101110'
+// 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.'
+console.log(decodeBits(FOX))
+console.log(decodeMorse(decodeBits(FOX)))
+// console.log(decodeMorse(decodeBits(E2)))
+// console.log(decodeMorse(decodeBits(E3)))
+// console.log(decodeMorse(decodeBits(heyJudeBits2)))
