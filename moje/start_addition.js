3ebe66960ec7b4f75b294e76d2971b9d1a628a74
mpiech1813 <maciejpiech18@gmail.com>
2023-12-11 12:29:22 -0600

    A

--- moje/start_addition.js
+++ moje/start_addition.js
@@ -5,7 +5,8 @@
  * X= 3 Y= 5 Z= 4 => no
  * X= 1 Y= 0.5 Z= 4 => yes
  * X= 1 Y= 0.3 Z= 4 => ?
- * A:
+ * A: create a for loop from x increasing by y until => Z. if === Z then return yes, if > then Z check 2 whether the last number before Z or the number after is closer to the Z.
+ *
  *
  *
  */
