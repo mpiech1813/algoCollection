723f240161f439e204af65dfc379461bb4819882
mpiech1813 <maciejpiech18@gmail.com>
2023-12-11 12:26:50 -0600

    RE

---
+++ moje/start_addition.js
@@ -0,0 +1,11 @@
+/**
+ * R: Given a starting number X, an increment Y and final number Z, write a function which will check if starting at X and increming by a number Y, will reach number Z. If it does not, was will be the closest number.
+ * E: X= 2 Y= 3 Z= 5 => yes
+ * X= 5 Y= 1 Z= 99 => yes
+ * X= 3 Y= 5 Z= 4 => no
+ * X= 1 Y= 0.5 Z= 4 => yes
+ * X= 1 Y= 0.3 Z= 4 => ?
+ * A:
+ *
+ *
+ */
