bd7070dcadc8b54a836b24ee7ad132e55a3110f9
mpiech1813 <maciejpiech18@gmail.com>
2024-07-15 12:52:03 -0500

    working on functionality

--- code_wars/createPhoneNumber.ts
+++ code_wars/createPhoneNumber.ts
@@ -2,11 +2,13 @@
 // E: createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 // A: check that there are no more or no less than 10 digits, check if all digits are numbers, create a template and return as string
 // C:
-function createPhoneNumber(number: number[]) {
+function createPhoneNumber(number) {
   if (number.length !== 10) throw "Check the number";
-  return `(${number.slice(0, 2)}) ${number.slice(3, 5)}-${number.slice(-6)}`;
+  const numbers = number.toString().replaceAll(",", "");
+  return `(${numbers.slice(0, 3)}) ${numbers.slice(3, 6)}-${numbers.slice(-6)}`;
 }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
-console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+// console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, "A", 7, 8, 9, 0]));
