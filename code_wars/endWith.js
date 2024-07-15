74d28946746d18120cc120fed4e7caa04f82536f
mpiech1813 <maciejpiech18@gmail.com>
2024-07-15 12:05:15 -0500

    basic bones

--- code_wars/endWith.js
+++ code_wars/endWith.js
@@ -3,7 +3,19 @@
 // A: slice string from end until ending -1. if slice === ending return true
 // C:
 
-function solution(str, ending) {}
+function solution(str, ending) {
+  if (str.length === 0 || ending.length === 0) return false;
+  const compare = str.slice(-ending.length);
+  console.log("compare:", compare);
+  console.log("ending:", ending);
 
+  if (compare === ending) {
+    return "true";
+  } else {
+    return "false";
+  }
+}
+
+// solution("abcde", "cde");
 console.log(solution("abcde", "cde"));
-console.log(solution("abcde", "abc"));
+// console.log(solution("abcde", "abc"));
