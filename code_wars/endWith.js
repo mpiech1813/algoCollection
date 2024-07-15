8af19b640506377cf0dcd5effa7ffae88267e9f4
mpiech1813 <maciejpiech18@gmail.com>
2024-07-15 11:57:10 -0500

    approach

---
+++ code_wars/endWith.js
@@ -0,0 +1,9 @@
+// R: create a function that takes a string and ending string. If string ends with ending string, return true
+// E: solution('abcde', 'cde'), true || solution('abcde', 'abc'), false
+// A: slice string from end until ending -1. if slice === ending return true
+// C:
+
+function solution(str, ending) {}
+
+console.log(solution("abcde", "cde"));
+console.log(solution("abcde", "abc"));
