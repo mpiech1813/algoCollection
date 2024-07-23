60eede9899f3966fffb0bfdee6982c808f69320e
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 10:09:59 -0500

    E

--- code_wars/square2.js
+++ code_wars/square2.js
@@ -1,4 +1,10 @@
 // R: create a function that checks if the number provided is square of a number
+// E: -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
 
 var isSquare = function (n) {
   return false; // fix me
