e0afa9b0fe0ef13ea02a77130f409bc69b87f832
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 11:12:10 -0500

    rough draft of the code

--- code_wars/persistantBugger.js
+++ code_wars/persistantBugger.js
@@ -4,3 +4,19 @@
 // A: Create an object that will hold itteration and current value, break the number into array, use reducer to multiply all of the numbers together, when done: add 1 to itterator and replace
 // current value with acc, keep repeating until current value.length === 1, return itterator
 // C:
+
+function persistence(num) {
+  const tracker = {
+    itteration: 0,
+    currentVal: num.toString().split(""),
+  };
+
+  while (tracker.currentVal.length > 1) {
+    const value = tracker.currentVal.reduce((acc, digit) => acc * digit);
+    tracker.currentVal = value.toString().split("");
+    tracker.itteration++;
+  }
+  return tracker.itteration;
+}
+
+console.log(persistence(18));
