e52251932a62c7ac526d55adf26b66ebc5c0283c
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 10:58:06 -0500

    working on C

--- code_wars/persistantBugger.js
+++ code_wars/persistantBugger.js
@@ -3,3 +3,4 @@
 // E: 39 => 3 || 999 => 4 || 4 =>0
 // A: Create an object that will hold itteration and current value, break the number into array, use reducer to multiply all of the numbers together, when done: add 1 to itterator and replace
 // current value with acc, keep repeating until current value.length === 1, return itterator
+// C:
