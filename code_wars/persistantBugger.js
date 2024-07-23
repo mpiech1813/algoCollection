fd60e9ef867dd0ab1d1228cef4f54ec295aa72a6
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 10:47:34 -0500

    working on A

--- code_wars/persistantBugger.js
+++ code_wars/persistantBugger.js
@@ -1,3 +1,4 @@
 // R: given a number, check and return the number of multiplicative persistances. Take digits of a number and multiply them together, keep repeating the process untill left with only oen digit,
 // record and return number of itterations
 // E: 39 => 3 || 999 => 4 || 4 =>0
+// A:
