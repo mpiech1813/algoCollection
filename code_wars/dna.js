e7d33f14ccad5eef058ccaa88cd7df5d5ed19e3e
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 10:36:23 -0500

    rough draft of C

--- code_wars/dna.js
+++ code_wars/dna.js
@@ -2,3 +2,17 @@
 // E: 'ATTGC' => 'TAACG' || 'GTAT' => 'CATA'
 // A: create an object with keys A,T,G,C and values as their matching pairs, conver string into an array, itterate through the array and replace each value with corresponding pair based on the object,
 // concat array into string and return. Will try to use the reducer.
+// C:
+
+function dnaStrand(dna) {
+  const pairs = {
+    A: "T",
+    T: "A",
+    G: "C",
+    C: "G",
+  };
+
+  return dna.split("").reducer((acc, letter) => {
+    acc + pairs[letter];
+  }, "");
+}
