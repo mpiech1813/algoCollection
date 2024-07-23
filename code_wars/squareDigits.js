e0cababe7ca6f11f3c33765cdc1ae6f69c7fbe74
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 08:45:10 -0500

    EA

--- code_wars/squareDigits.js
+++ code_wars/squareDigits.js
@@ -1,4 +1,6 @@
 // R: take a number and square every digit in it, return the squares concatenated as number
+// E: num = 9119 => 811181 || num =765 => 493625
+// A: convert number into an array, may have to convert it into a string first, each element in the array get multiplied by itself, concatenate into string, change into number
 
 function squareDigits(num) {
   return 0;
