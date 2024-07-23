ddd1edb026e33f6839cc11ffc9d5bcbe3fe46862
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 11:14:31 -0500

    added cleaner example

--- code_wars/persistantBugger.js
+++ code_wars/persistantBugger.js
@@ -23,3 +23,20 @@ console.log(persistence(18));
 console.log(persistence(39));
 console.log(persistence(999));
 console.log(persistence(4));
+
+function persistenceCopy(num) {
+  var times = 0;
+
+  num = num.toString();
+
+  while (num.length > 1) {
+    times++;
+    num = num
+      .split("")
+      .map(Number)
+      .reduce((a, b) => a * b)
+      .toString();
+  }
+
+  return times;
+}
