f47e07bf5c5eb0ab8f26e2d600b4b556ca47e349
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 10:57:51 -0500

    general idea of A

--- code_wars/persistantBugger.js
+++ code_wars/persistantBugger.js
@@ -1,4 +1,5 @@
 // R: given a number, check and return the number of multiplicative persistances. Take digits of a number and multiply them together, keep repeating the process untill left with only oen digit,
 // record and return number of itterations
 // E: 39 => 3 || 999 => 4 || 4 =>0
-// A:
+// A: Create an object that will hold itteration and current value, break the number into array, use reducer to multiply all of the numbers together, when done: add 1 to itterator and replace
+// current value with acc, keep repeating until current value.length === 1, return itterator
