f493ea2c4f606552920867482678a5eda7dcf0ff
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 10:34:09 -0500

    A

--- code_wars/dna.js
+++ code_wars/dna.js
@@ -1,3 +1,4 @@
 // R: give a string of parts of DNA composed of 'A', 'T', 'G', 'C', create a function that returns a matching pairs
 // E: 'ATTGC' => 'TAACG' || 'GTAT' => 'CATA'
-// A:
+// A: create an object with keys A,T,G,C and values as their matching pairs, conver string into an array, itterate through the array and replace each value with corresponding pair based on the object,
+// concat array into string and return. Will try to use the reducer.
