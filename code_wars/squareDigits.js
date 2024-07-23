cc402ae61233fa7dc2e9724cbe5956e60cf32369
mpiech1813 <maciejpiech18@gmail.com>
2024-07-23 09:58:14 -0500

    solved

--- code_wars/squareDigits.js
+++ code_wars/squareDigits.js
@@ -1,7 +1,20 @@
 // R: take a number and square every digit in it, return the squares concatenated as number
 // E: num = 9119 => 811181 || num =765 => 493625
-// A: convert number into an array, may have to convert it into a string first, each element in the array get multiplied by itself, concatenate into string, change into number
+// A: convert number into an array, may have to convert it into a string first, each element in the array get multiplied by itself, concatenate into string, change into number (parseInt())
+// C:
 
 function squareDigits(num) {
-  return 0;
+  const numArr = num
+    .toString()
+    .split("")
+    .map((ele) => Math.pow(ele, 2))
+    .join("");
+
+  //   return num
+  //     .toString()
+  //     .split("")
+
+  return parseInt(numArr);
 }
+
+console.log(squareDigits(1123));
