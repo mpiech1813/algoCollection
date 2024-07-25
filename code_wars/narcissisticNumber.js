48cdc190214b677fd94bd3aec9488e57e13728ae
mpiech1813 <maciejpiech18@gmail.com>
2024-07-25 10:30:54 -0500

    A

--- code_wars/narcissisticNumber.js
+++ code_wars/narcissisticNumber.js
@@ -1,3 +1,5 @@
 // R: Write a function which checks if the given number is a narcissistic number. The sum of all of its digits
 // raised to the power of total number of digits has to equal to the number.
 // E: 153 =>  1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153 || 7 => 7^1 =7
+// A: Break the number into an array, check if length is 1, yes return true, establish length of array (pwr),
+// use reducer to calculate the total acc + ele^pwr, check if acc === original number
