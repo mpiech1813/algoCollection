fc5a3259d4eb9d0a66f2778c2a4a891baae52a93
mpiech1813 <maciejpiech18@gmail.com>
2024-07-25 10:27:23 -0500

    E

--- code_wars/narcissisticNumber.js
+++ code_wars/narcissisticNumber.js
@@ -1,2 +1,3 @@
 // R: Write a function which checks if the given number is a narcissistic number. The sum of all of its digits
 // raised to the power of total number of digits has to equal to the number.
+// E: 153 =>  1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153 || 7 => 7^1 =7
