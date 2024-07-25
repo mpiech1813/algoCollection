0a97a128ada3085676d1c122ef371f7f9d5fa5e8
mpiech1813 <maciejpiech18@gmail.com>
2024-07-25 10:27:09 -0500

    R

---
+++ code_wars/narcissisticNumber.js
@@ -0,0 +1,2 @@
+// R: Write a function which checks if the given number is a narcissistic number. The sum of all of its digits
+// raised to the power of total number of digits has to equal to the number.
