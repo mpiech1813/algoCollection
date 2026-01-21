955d9fcfbbae33b2012f9745eac7a27f5d5232b6
mpiech1813 <maciejpiech18@gmail.com>
2026-01-21 12:04:09 -0600

    Repeat

---
+++ code_wars/multiple35.js
@@ -0,0 +1,5 @@
+// R: Write a function which takes a number, checks all of the natural numbers from 1 input number,
+// if divisible by 3 or 5, add and sum (useReducer?)
+// return sum
+
+function solution(num) {}
