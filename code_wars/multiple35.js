0a78e78374b51a11263389265080724f1b2e0ed4
mpiech1813 <maciejpiech18@gmail.com>
2026-01-21 12:41:38 -0600

    better working version

--- code_wars/multiple35.js
+++ code_wars/multiple35.js
@@ -1,6 +1,7 @@
 // R: Write a function which takes a number, checks all of the natural numbers from 1 input number,
 // if divisible by 3 or 5, add and sum (useReducer?)
 // return sum
+// if number is negative, return 0
 // E: num = 10
 // natural numbers : 3, 5, 6, 9
 // sum: 23
@@ -10,17 +11,20 @@
 // if not divisible by 3, check if divisible by 5 and add to sum
 // if not divisible by both, skip
 // return sum
+// check if number is negative
 // C:
 
 function solution(num) {
-  const numsArr = [3];
+  const numsArr = Array.from({ length: num - 1 }, (_, index) => ++index);
 
-  while (numsArr[numsArr.length - 1] < num) {
-    const lastItem = numsArr[numsArr.length - 1];
-    numsArr.push(lastItem + 1);
+  function reduceFunction(total, currNum) {
+    if (currNum % 3 === 0 || currNum % 5 === 0) {
+      return total + currNum;
+    }
+    return total;
   }
 
-  return numsArr;
+  return numsArr.reduce(reduceFunction, 0);
 }
 
-console.log("array is: ", solution(10));
+console.log("array is: ", solution(100));
