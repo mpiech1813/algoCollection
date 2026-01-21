af7fa1e12f487271cf20e8e8103b6311e6426482
mpiech1813 <maciejpiech18@gmail.com>
2026-01-21 12:05:15 -0600

    example

--- code_wars/multiple35.js
+++ code_wars/multiple35.js
@@ -1,5 +1,9 @@
 // R: Write a function which takes a number, checks all of the natural numbers from 1 input number,
 // if divisible by 3 or 5, add and sum (useReducer?)
 // return sum
+// E: num = 10
+// natural numbers : 3, 5, 6, 9
+// sum: 23
+// A:
 
 function solution(num) {}
