708f6939bd46bc7734b34dc054f0679d1283dbc4
mpiech1813 <maciejpiech18@gmail.com>
2026-01-23 13:07:45 -0600

    basic code

--- code_wars/duplicateEncoder.js
+++ code_wars/duplicateEncoder.js
@@ -14,3 +14,32 @@
 // create a new array and itterate through characters array with map. Compare each character with the object and
 // return '(' or ')' accordingly
 // convert the final array into string and return
+// C:
+
+function duplicateEncode(word) {
+  const characterCount = {};
+  const characterArray = Array.from(word.toLowerCase());
+  characterArray.forEach((char) => {
+    if (!characterCount[char]) {
+      characterCount[char] = 1;
+    } else {
+      characterCount[char] = ++characterCount[char];
+    }
+  });
+
+  const finalArray = characterArray
+    .map((char) => {
+      if (characterCount[char] === 1) return "(";
+
+      return ")";
+    })
+    .join("");
+
+  return finalArray;
+}
+
+console.log("din", duplicateEncode("din"));
+console.log("recede", duplicateEncode("recede"));
+console.log("Success", duplicateEncode("Success"));
+console.log("(( @", duplicateEncode("(( @"));
+console.log("hElLo w()orld", duplicateEncode("hElLo w()orld"));
